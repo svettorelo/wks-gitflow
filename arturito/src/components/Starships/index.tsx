99a5790b93e75fe1f0b672a7a2bc5f7a278224bc
svettorelo <sabrinavettorelo@gmail.com>
2021-10-20 16:31:41 -0300

    Starships component added

---
+++ arturito/src/components/Starships/index.tsx
@@ -0,0 +1,50 @@
+import Table from '../Table';
+import useSWR from 'swr';
+import { swGet } from '../../utils/fetcher';
+
+const columns = [
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+    },
+    {
+        title: 'Model',
+        dataIndex: 'model',
+        key: 'model',
+    },
+    {
+        title: 'Manufacturer',
+        dataIndex: 'manufacturer',
+        key: 'manufacturer',
+    },
+    {
+        title: 'Passengers',
+        dataIndex: 'passengers',
+        key: 'passengers',
+        render: (passengers: string) =>
+            parseInt(passengers)
+                ? parseInt(passengers).toLocaleString('es-AR')
+                : passengers,
+    },
+    {
+        title: '# films',
+        dataIndex: 'films',
+        key: 'films_count',
+        render: (films: string[]) => films.length,
+    },
+];
+function Starships (){
+    const { data, error } = useSWR('/starships', swGet);
+    if (error) {
+        return <div className="px-2">Oh oh!</div>;
+    }
+    if (!data) {
+        return <div className="px-2">Loading...</div>;
+    }
+
+    return (<div>
+        <Table columns={columns} data={data.results} />
+    </div>)
+}
+export default Starships;
